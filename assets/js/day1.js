cbd8a73cbc17fd841ec268b6b817186457861483
Alex McDonald <alex.mcdonald@informationcatalyt.com>
2021-12-02 01:00:04 +0000

    part 2: three measure system

--- assets/js/day1.js
+++ assets/js/day1.js
@@ -12,6 +12,7 @@ var inputElement = document.getElementById('day1Input');
 
 //The output span
 var day1ResultElement = document.getElementById('day1Result');
+var day1ResultElement2 = document.getElementById('day1Result2');
 
 function SonarSweep(){
     //Get the raw output
@@ -59,4 +60,70 @@ function SonarSweep(){
 
     // Set the styling appropriately
     day1ResultCard.className = 'card text-white bg-success mb-3 visible';
+
+    //Trigger Part 2
+    SonarSweepThreeMeasurement();
+}
+
+function SonarSweepThreeMeasurement(){
+    //Get the raw output
+    rawInput = inputElement.value;
+    result = 0;
+    tempPrev = '';
+
+    //Adding the results together
+    var tempA = 0;
+    var counterThree = 0;
+
+    //counts
+    var countDecreased = 0;
+    var countNeither = 0;
+    var countNoChange = 0;
+
+    //Split the input into an array
+    const inputArray = rawInput.split("\n");
+    console.log("\nThree Measure:");
+    console.log("Amount of entries: " + inputArray.length);
+
+    //Go through the array
+    for (var i = 2; i < inputArray.length; i++){
+        //Add an compare
+        tempA = parseInt(inputArray[i]) + parseInt(inputArray[i-1]) + parseInt(inputArray[i-2]);
+            
+        //We only care about things in groups of threes
+        if (i == 2){
+            //console.log(tempA + " (N/A no previous measurement)");
+            countNeither++;
+        }
+        else if (parseInt(tempA) > parseInt(tempPrev)) {
+            //The measurement has increased since the last and its not the first value!
+            //console.log(tempA + " (increased)");
+            result++;
+        }
+        else if (parseInt(tempA) < parseInt(tempPrev)) {
+            //console.log(tempA + " (decreased)");
+            countDecreased++;
+        }
+        else if (parseInt(tempA) == parseInt(tempPrev)) {
+            //console.log(tempA + " (no change)");
+            countNoChange++;
+        }
+        else {
+            //console.log(inputArray[i] + " ERROR");
+            countNeither++;
+        }
+        //console.log(inputArray[i]);
+        tempPrev = tempA;
+    }
+    
+    //Output the result for the user!
+    console.log("Three Measurement, Amount of increases: " + result);
+    console.log("Three Measurement, Amount of decreases: " + countDecreased);
+    console.log("Three Measurement, Amount of no changes: " + countNoChange);
+    console.log("Three Measurement, Amount of non results: " + countNeither);
+    //console.log("Final number should be: " + (10139+10179+10180))
+    day1ResultElement2.textContent = "There are " + result + " measurements that are larger than previous measurements!";
+
+    // Set the styling appropriately
+    day1ResultCard2.className = 'card text-white bg-success mb-3 visible';
 }
